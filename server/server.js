3cfe97e0327de7170889877e9074b7c18da585bf
Rahul Singh <rahulsinghrajpoot9997@gmail.com>
2026-08-16 14:56:19 +0530

    Fix trailing slash in CORS origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', "https://job-tracker-delta-ivory.vercel.app/"],
+    origin: ["http://localhost:5173", "https://job-tracker-delta-ivory.vercel.app"],
     credentials: true,
   }),
 );
